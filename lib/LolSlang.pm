2dec13fdc08d9af96fda3586478c2d03af0c393d
Carl Masak <cmasak@gmail.com>
2014-08-23 12:25:33 +0200

    can just return a literal hash here

--- lib/LolSlang.pm
+++ lib/LolSlang.pm
@@ -10,5 +10,5 @@ sub EXPORT(*@a) {
     $*W.install_lexical_symbol($*W.cur_lexpad(), '%?LANG', $*W.p6ize_recursive(%*LANG));
     $*W.install_lexical_symbol($*W.cur_lexpad(), '$*MAIN', $*W.p6ize_recursive($*MAIN));
 
-    $*W.p6ize_recursive( nqp::hash() );
+    {};
 }
